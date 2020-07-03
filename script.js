3bdb88b71f981a47a24a91c8d71727b3f3b9a6a1
SnehaPandit10 <61976632+SnehaPandit10@users.noreply.github.com>
2020-07-03 20:48:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-var i=0;
-var l=names.length;
-for( i=0;i<l;i++)
-{
-    var n=names[i];
-    var s=n.charAt(0);
-    if(s!='J' && s!='j')
-    {
-        speakk(n);
 
+(function (window){
+    
+    
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
+    
+    for (var i in names) {
+    
+      
+       var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+    
+     
+      if (firstLetter == 'j') {
+        window.byeSpeaker.speak(names[i]);
+      } else {
+        window.helloSpeaker.speak(names[i]);
+      }
     }
-    else{
-        speak(n);
-    }
-
-}
+    })(window);
